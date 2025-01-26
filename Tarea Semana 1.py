3dcc4791d1defdd49d9a00a5456b7ebbbc6c5928
ErosRata <xxerosrataxx@gmail.com>
2025-01-26 15:13:09 -0600

    Agregar Tarea Semana 1.py

---
+++ Tarea Semana 1.py
@@ -0,0 +1,30 @@
+#1 la respuesta es el inciso C
+
+#2 Programa: una secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo.
+
+#3 Intérprete: lee el código fuente de los programas tal y como ha sido escrito por el programador, lo analiza, e interpreta sus instrucciones sobre la marcha. 
+# Compilador: necesita que le entreguen el programa completo en un fichero, y luego ejecuta un proceso para traducir el código fuente de alto nivel a código máquina
+
+#4 la respuesta es el inciso C
+
+#5 esta mal escrito el "print" en especifico la 'n' que en su lugar esta una 'm', le falta el parentesis, y la comillas dobles; osea esta mal la sintaxis, y ese error ocurre en 
+# la 1ra y 3ra linea, en la segundo la verdad no se cual el error.
+
+#6 la respuesta es el inciso C
+
+#7 la respuesta es el inciso B
+x = 43
+x = x + 1
+print(x)
+
+#8 
+#Unidad central de procesamiento: este simplemente es el cerebro y va indicar que hacer ante ciertas situaciones y procesa toda la informacion.
+#Memoria principal:  es como cuando tomas una clase y saca tus lapiz y tu borrador para tomar apuntes, pero al concluir las clases, lo guardas en tu mochila, 
+# hasta que tomes nuevamente una clases, osea que esta al alcance en ese momento, hasta el final ya no lo vas a ocupar.
+#Memoria secundaria: es el informacion que guardas para siempre, esta te va decir que saques el lapiz y el borrador ya que vas tomar apuntes, osea jamas se te va olvidar 
+# porque desde que eres un nino sabes que ocupas eso para tomar la clase.
+#Dispositivos de entrada: se podria relacionar con los 5 sentidos, ya que con esos recibimos la informacion, si algo esta caliente o frio, grande o pequeno, huele bien o mal, etc...
+#Dispositivos de salida: esto se relaciona con ideas o pensamientos que hacemos con la informacion que nos llega con los dispositivos de entrada, hasta podria llegar reaccionar 
+# con movimientos o cualquier accion.
+
+#9 pues buscando el error como si fuera un error ortografico, osea buscar, por decirlo asi, donde le falta el acento o tilde, o cuando pones una b en vez de una v, etc...
