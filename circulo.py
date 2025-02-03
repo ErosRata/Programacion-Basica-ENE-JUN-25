95f482ee6f3e1a7c27eb34b399a893dc732effd8
ErosRata <xxerosrataxx@gmail.com>
2025-02-03 13:13:32 -0600

    circulo

---
+++ circulo.py
@@ -0,0 +1,7 @@
+radio = float(input("Ingrese el radio del circulo: "))
+pi = 3.1416
+
+area = pi * radio**2
+circuferencia = (2 * pi) * radio
+
+print("La circuferencia es de:",circuferencia,"y el area de:",area,)
