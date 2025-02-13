dce3f2e2bacd9b786b52c4d6d9c71a29a9ff5612
ErosRata <xxerosrataxx@gmail.com>
2025-02-12 20:06:23 -0600

    cel

--- Problemas del 10-20/(E13) temperatura.py
+++ Problemas del 10-20/(E13) temperatura.py
@@ -1,8 +1,6 @@
-tipo = int(input('Que tipo de temperatura vas a convertir (celsius=1), (fahrenheit=2), (kelvin=3): '))
-F = []
-K = []
-if tipo == 1:
-    C = float(input('Introduce la cantidad a convertir: '))
-    F = (C * 1.8) + 32
-    K = C + 273.15
-else: print('En fahrenheit:', F, 'y en Kelvin:', K)
+c = float(input("Ingresa la temperatura en celsius: "))
+
+f = (c * 1.8) + 32
+k = c + 273.15
+
+print("En fahrenheit es:",f,"y en kelvin son:",k)
