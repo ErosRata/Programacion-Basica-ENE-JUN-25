fc44059f23dcc0635d9fc84a844e8ae5334d1478
Laptop Ratona <xxerosrataxx@gmail.com>
2025-02-18 19:36:51 -0600

    listo

--- Problemas 21-30/(E22) dado y moneda.py
+++ Problemas 21-30/(E22) dado y moneda.py
@@ -3,13 +3,13 @@ import random
 jugar = input("Cual juego quieres jugar? (Moneda = M, m o 1; Dado = D, d o 2): ")
 numero = []
 
-if jugar == 'M':
+if jugar == 'M' or jugar == 'm' or jugar == '1':
     numero = random.randrange(1,2)
     if numero == 1:
         print("Aguila")
     else:
         print("Sol")
 else:
-    jugar == "D"
+    jugar == "D" or jugar == 'd' or jugar == '2'
     numero = random.randrange(1,6)
     print(numero)
